637e8d0f66820e715f68e3616fad5ce782eae6ac
Maureen Cheptoo <cheptoomaureen31@gmail.com>
2024-03-22 22:36:59 +0300

    assignment

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
 const returnFirstTwoDrivers = function (drivers) {
     const twoDrivers = drivers.slice(0, 2);
     return twoDrivers;
-  };
+  }
   const returnLastTwoDrivers = function (drivers) {
     const lastDrivers = drivers.slice(-2);
     return lastDrivers;
-  };
+  }
   const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
   function createFareMultiplier(interger) {
     const fareMultiplier = fare => fare * interger;
